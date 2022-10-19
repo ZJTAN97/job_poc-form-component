df2191d29cb2f68e3fe59f991ee36f88288ef3f9
ZJ <zhijietan97@gmail.com>
2022-10-19 22:57:14 +0800

    chore: research on zod

--- src/pages/CreateProfile/index.tsx
+++ src/pages/CreateProfile/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import {
-  useForm,
-  useController,
-  FormProvider,
-  SubmitHandler,
+    useForm,
+    useController,
+    FormProvider,
+    SubmitHandler,
 } from "react-hook-form";
 import { FormSchemaType, formSchema } from "../../validations";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -12,73 +12,81 @@ import mockSearchCharacterNames from "../../mock";
 import { debounce } from "lodash";
 
 const CreateProfile: React.FC = () => {
-  const methods = useForm<FormSchemaType>({
-    resolver: zodResolver(formSchema),
-    mode: "onChange",
-    defaultValues: {
-      job: "HERO",
-      characterName: "",
-    },
-  });
+    const methods = useForm<FormSchemaType>({
+        resolver: zodResolver(formSchema),
+        mode: "onChange",
+        defaultValues: {
+            job: "HERO",
+            characterName: "",
+        },
+    });
 
-  const { watch, formState, handleSubmit, control } = methods;
-  const { isDirty, isValid, isSubmitting } = formState;
+    const { watch, formState, handleSubmit, control } = methods;
+    const { isDirty, isValid, isSubmitting, errors } = formState;
 
-  const { field: jobField } = useController({ name: "job", control: control });
-  const { field: characterNameField } = useController({
-    name: "characterName",
-    control: control,
-  });
+    const { field: jobField } = useController({
+        name: "job",
+        control: control,
+    });
+    const { field: characterNameField } = useController({
+        name: "characterName",
+        control: control,
+    });
 
-  const [characterNameError, setCharacterNameError] = React.useState("");
+    const [characterNameError, setCharacterNameError] = React.useState("");
 
-  const debouncedSearch = debounce(async (criteria: string) => {
-    await mockSearchCharacterNames(criteria, setCharacterNameError);
-  }, 750);
+    const debouncedSearch = debounce(async (criteria: string) => {
+        await mockSearchCharacterNames(criteria, setCharacterNameError);
+    }, 750);
 
-  const searchExistingNames = React.useCallback((searchValue: string) => {
-    characterNameField.onChange(searchValue);
-    debouncedSearch(searchValue);
-  }, []);
+    const searchExistingNames = (searchValue: string) => {
+        characterNameField.onChange(searchValue);
+        debouncedSearch(searchValue);
+    };
 
-  const submitForm = handleSubmit(async (data) => {
-    console.log(data);
-  });
+    const submitForm = handleSubmit(async (data) => {
+        console.log(data);
+    });
 
-  return (
-    <div>
-      <div>Select Maplestory Job Type</div>
-      <div>Fill in the mandatory fields below</div>
+    console.log("-- current errors --");
+    console.log(errors);
 
-      <FormProvider {...methods}>
-        <Chip
-          checked={jobField.value === "HERO"}
-          onChange={() => jobField.onChange("HERO")}
-        >
-          Hero
-        </Chip>
-        <Chip
-          checked={jobField.value === "ADVENTURER"}
-          onChange={() => jobField.onChange("ADVENTURER")}
-        >
-          Adventurer
-        </Chip>
-        <TextInput
-          value={characterNameField.value}
-          onChange={(e) => searchExistingNames(e.target.value)}
-          error={
-            characterNameError && <a href={characterNameField.value}>Link</a>
-          }
-        />
-        <Button
-          disabled={characterNameError ? true : false}
-          onClick={submitForm}
-        >
-          Create Character
-        </Button>
-      </FormProvider>
-    </div>
-  );
+    return (
+        <div>
+            <div>Select Maplestory Job Type</div>
+            <div>Fill in the mandatory fields below</div>
+
+            <FormProvider {...methods}>
+                <Chip
+                    checked={jobField.value === "HERO"}
+                    onChange={() => jobField.onChange("HERO")}
+                >
+                    Hero
+                </Chip>
+                <Chip
+                    checked={jobField.value === "ADVENTURER"}
+                    onChange={() => jobField.onChange("ADVENTURER")}
+                >
+                    Adventurer
+                </Chip>
+                <TextInput
+                    value={characterNameField.value}
+                    onChange={(e) => searchExistingNames(e.target.value)}
+                    error={
+                        characterNameError && (
+                            <a href={characterNameField.value}>Link</a>
+                        )
+                    }
+                />
+                <Button
+                    disabled={characterNameError ? true : false}
+                    onClick={submitForm}
+                >
+                    Create Character
+                </Button>
+            </FormProvider>
+        </div>
+    );
 };
 
 export default CreateProfile;
--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,8 +1,11 @@
 import z from "zod";
 
 export const formSchema = z.object({
-  job: z.enum(["HERO", "ADVENTURER"]),
-  characterName: z.string(),
+    job: z.enum(["HERO", "ADVENTURER"]),
+    characterName: z.string().min(5, "Minimum 5 characters"),
 });
 
 export type FormSchemaType = z.infer<typeof formSchema>;
+
+// TODO:
+// Research if zod is meant for form validation or just for schema validation
