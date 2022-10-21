eea1c3485f3810c52c6e164547b64ee113afb2fe
ZJ <zhijietan97@gmail.com>
2022-10-21 11:09:24 +0800

    refactor: naming convention

--- src/pages/CreateProfile/index.tsx
+++ src/pages/CreateProfile/index.tsx
@@ -6,112 +6,118 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Chip, Textarea } from "@mantine/core";
 import mockSearchCharacterNames from "../../mock";
 import { debounce } from "lodash";
-import CesForm from "../../components/CesForm";
+import Form from "../../components/Form";
 import { useNavigate } from "@tanstack/react-location";
 
 const CreateProfile: React.FC = () => {
-  const navigate = useNavigate();
-  const methods = useForm<FormSchemaType>({
-    resolver: zodResolver(formSchema),
-    mode: "onChange",
-    defaultValues: {
-      job: "HERO",
-      characterName: "",
-      bio: "",
-      dateCreated: new Date(),
-    },
-  });
+    const navigate = useNavigate();
+    const methods = useForm<FormSchemaType>({
+        resolver: zodResolver(formSchema),
+        mode: "onChange",
+        defaultValues: {
+            job: "HERO",
+            characterName: "",
+            bio: "",
+            dateCreated: new Date(),
+        },
+    });
 
-  const { formState, handleSubmit, control, reset } = methods;
-  const { isDirty, isValid, isSubmitting } = formState;
+    const { formState, handleSubmit, control, reset } = methods;
+    const { isDirty, isValid, isSubmitting } = formState;
 
-  const { field: jobField } = useController({
-    name: "job",
-    control,
-  });
-  const { field: characterNameField } = useController({
-    name: "characterName",
-    control,
-  });
-  const { field: bioField } = useController({
-    name: "bio",
-    control,
-  });
+    const { field: jobField } = useController({
+        name: "job",
+        control,
+    });
+    const { field: characterNameField } = useController({
+        name: "characterName",
+        control,
+    });
+    const { field: bioField } = useController({
+        name: "bio",
+        control,
+    });
 
-  const [characterNameError, setCharacterNameError] = React.useState("");
+    const [characterNameError, setCharacterNameError] = React.useState("");
 
-  const debouncedSearch = debounce((criteria: string) => {
-    mockSearchCharacterNames(criteria, setCharacterNameError);
-  }, 500);
+    const debouncedSearch = debounce((criteria: string) => {
+        mockSearchCharacterNames(criteria, setCharacterNameError);
+    }, 500);
 
-  const switchJobType = (jobType: FormSchemaType["job"]) => {
-    characterNameField.onChange("");
-    bioField.onChange("");
-    jobField.onChange(jobType);
-    // reset();
-  };
+    const switchJobType = (jobType: FormSchemaType["job"]) => {
+        characterNameField.onChange("");
+        bioField.onChange("");
+        jobField.onChange(jobType);
+        // reset();
+    };
 
-  const submitForm = handleSubmit(async (data) => {
-    console.log(data);
-    navigate({ to: "/profile", replace: true });
-  });
+    const submitForm = handleSubmit(async (data) => {
+        console.log(data);
+        navigate({ to: "/profile", replace: true });
+    });
 
-  return (
-    <div className="main__container">
-      <div>
-        <div className="title">Select Maplestory Job Type</div>
-        <div className="title">Fill in the mandatory fields below</div>
+    return (
+        <div className="main__container">
+            <div>
+                <div className="title">Select Maplestory Job Type</div>
+                <div className="title">Fill in the mandatory fields below</div>
 
-        <CesForm methods={methods} useLocalStorage={true} preventLeaving={true}>
-          <div className="chip__row">
-            <Chip
-              disabled={isSubmitting}
-              checked={jobField.value === "HERO"}
-              onChange={() => switchJobType("HERO")}
-            >
-              Hero
-            </Chip>
-            <Chip
-              disabled={isSubmitting}
-              checked={jobField.value === "ADVENTURER"}
-              onChange={() => switchJobType("ADVENTURER")}
-            >
-              Adventurer
-            </Chip>
-          </div>
+                <Form
+                    methods={methods}
+                    useLocalStorage={true}
+                    preventLeaving={true}
+                >
+                    <div className="chip__row">
+                        <Chip
+                            disabled={isSubmitting}
+                            checked={jobField.value === "HERO"}
+                            onChange={() => switchJobType("HERO")}
+                        >
+                            Hero
+                        </Chip>
+                        <Chip
+                            disabled={isSubmitting}
+                            checked={jobField.value === "ADVENTURER"}
+                            onChange={() => switchJobType("ADVENTURER")}
+                        >
+                            Adventurer
+                        </Chip>
+                    </div>
 
-          <CesForm.TextInput
-            disabled={isSubmitting}
-            label={"Character Name"}
-            control={control}
-            name={"characterName"}
-            additionalCallBacks={debouncedSearch}
-            error={
-              characterNameError && (
-                <a href={"/profile"}>This name exists, do you mean this guy?</a>
-              )
-            }
-          />
+                    <Form.TextInput
+                        disabled={isSubmitting}
+                        label={"Character Name"}
+                        control={control}
+                        name={"characterName"}
+                        additionalCallBacks={debouncedSearch}
+                        error={
+                            characterNameError && (
+                                <a href={"/profile"}>
+                                    This name exists, do you mean this guy?
+                                </a>
+                            )
+                        }
+                    />
 
-          <Textarea
-            disabled={isSubmitting}
-            label={"Bio for Character"}
-            className="input"
-            value={bioField.value}
-            onChange={(e) => bioField.onChange(e.target.value)}
-          />
+                    <Textarea
+                        disabled={isSubmitting}
+                        label={"Bio for Character"}
+                        className="input"
+                        value={bioField.value}
+                        onChange={(e) => bioField.onChange(e.target.value)}
+                    />
 
-          <Button
-            disabled={!isValid}
-            onClick={submitForm}
-            className="create__btn"
-          >
-            Create Character
-          </Button>
-        </CesForm>
-      </div>
-    </div>
-  );
+                    <Button
+                        disabled={!isValid}
+                        onClick={submitForm}
+                        className="create__btn"
+                    >
+                        Create Character
+                    </Button>
+                </Form>
+            </div>
+        </div>
+    );
 };
 
 export default CreateProfile;
--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,22 +1,22 @@
 import z from "zod";
 
 export const baseSchema = z.object({
-  bio: z.string().min(10, "Minimally 10 character"),
-  dateCreated: z.date(),
+    bio: z.string().min(10, "Minimally 10 character"),
+    dateCreated: z.date(),
 });
 
 export const formSchemaHero = z.object({
-  job: z.literal("HERO"),
-  characterName: z.string().optional(),
+    job: z.literal("HERO"),
+    characterName: z.string().optional(),
 });
 
 export const formSchemaAdventurer = z.object({
-  job: z.literal("ADVENTURER"),
-  characterName: z.string().min(5, "require at least 5 characters"),
+    job: z.literal("ADVENTURER"),
+    characterName: z.string().min(5, "require at least 5 characters"),
 });
 
 export const formSchema = z
-  .union([formSchemaHero, formSchemaAdventurer])
-  .and(baseSchema); // extends base schema
+    .union([formSchemaHero, formSchemaAdventurer])
+    .and(baseSchema); // extends base schema
 
 export type FormSchemaType = z.infer<typeof formSchema>;
