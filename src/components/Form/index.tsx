7ab25d28865d02b3a1afe264a5c0897cd71904e1
ZJ <zhijietan97@gmail.com>
2022-10-25 18:10:19 +0800

    chore: remove unused props

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,60 +1,50 @@
-import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
-import { FormProvider, useForm, UseFormReturn } from "react-hook-form";
+import { FormProvider, UseFormReturn } from "react-hook-form";
 import { ChipSelection } from "./components/ChipSelection";
 import { TextArea } from "./components/TextArea";
 import { TextInput } from "./components/TextInput";
 
-interface FormProps<T> {
-    methods: UseFormReturn<any>;
-    useLocalStorage: boolean;
-    preventLeaving: boolean;
-    children: React.ReactNode;
-    // testing feasibility of including schema here
-    formSchema: any;
+interface FormProps {
+  methods: UseFormReturn<any>;
+  useLocalStorage: boolean;
+  preventLeaving: boolean;
+  children: React.ReactNode;
 }
 
-const Form = <T,>({
-    methods,
-    useLocalStorage,
-    preventLeaving,
-    children,
-    formSchema,
-}: FormProps<T>) => {
-    const { formState } = methods;
-    const { isDirty } = formState;
+const Form = ({
+  methods,
+  useLocalStorage,
+  preventLeaving,
+  children,
+}: FormProps) => {
+  const { formState } = methods;
+  const { isDirty } = formState;
 
-    // still testing the feasibility of this method
-    const methods2 = useForm<typeof formSchema>({
-        resolver: zodResolver(formSchema),
-        mode: "onChange",
-    });
+  if (useLocalStorage) {
+    // TODO: add localstorage logic here
+  }
 
-    if (useLocalStorage) {
-        // TODO: add localstorage logic here
-    }
+  // Wrapped in useCallback to ensure its the same reference function
+  // as useEffect dependency array has beforeUnload
+  // prevents side effects re-rendering when updating form
+  const beforeUnload = React.useCallback(
+    (event: BeforeUnloadEvent) => {
+      if (preventLeaving && isDirty) {
+        event.returnValue = true;
+      }
+    },
+    [isDirty, preventLeaving]
+  );
 
-    // Wrapped in useCallback to ensure its the same reference function
-    // as useEffect dependency array has beforeUnload
-    // prevents side effects re-rendering when updating form
-    const beforeUnload = React.useCallback(
-        (event: BeforeUnloadEvent) => {
-            if (preventLeaving && isDirty) {
-                event.returnValue = true;
-            }
-        },
-        [isDirty, preventLeaving]
-    );
+  React.useEffect(() => {
+    console.log("[INFO] Side effect triggered");
+    window.addEventListener("beforeunload", beforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", beforeUnload);
+    };
+  }, [beforeUnload]);
 
-    React.useEffect(() => {
-        console.log("[INFO] Side effect triggered");
-        window.addEventListener("beforeunload", beforeUnload);
-        return () => {
-            window.removeEventListener("beforeunload", beforeUnload);
-        };
-    }, [beforeUnload]);
-
-    return <FormProvider {...methods}>{children}</FormProvider>;
+  return <FormProvider {...methods}>{children}</FormProvider>;
 };
 
 Form.TextInput = TextInput;
--- src/pages/CreateProfile/index.tsx
+++ src/pages/CreateProfile/index.tsx
@@ -10,101 +10,96 @@ import Form from "../../components/Form";
 import { useNavigate } from "@tanstack/react-location";
 
 const CreateProfile: React.FC = () => {
-    const navigate = useNavigate();
-    const methods = useForm<FormSchemaType>({
-        resolver: zodResolver(formSchema),
-        mode: "onChange",
-        defaultValues: {
-            job: "HERO",
-            characterName: "",
-            bio: "",
-            dateCreated: new Date(),
-            password: "",
-            confirmPassword: "",
-        },
-    });
+  const navigate = useNavigate();
+  const methods = useForm<FormSchemaType>({
+    resolver: zodResolver(formSchema),
+    mode: "onChange",
+    defaultValues: {
+      job: "HERO",
+      characterName: "",
+      bio: "",
+      dateCreated: new Date(),
+      password: "",
+      confirmPassword: "",
+    },
+  });
 
-    const { formState, handleSubmit, control } = methods;
-    const { isValid, isSubmitting } = formState;
+  const { formState, handleSubmit, control } = methods;
+  const { isValid, isSubmitting } = formState;
 
-    const [characterNameError, setCharacterNameError] = React.useState("");
+  const [characterNameError, setCharacterNameError] = React.useState("");
 
-    const debouncedSearch = debounce((criteria: string) => {
-        mockSearchCharacterNames(criteria, setCharacterNameError);
-    }, 500);
+  const debouncedSearch = debounce((criteria: string) => {
+    mockSearchCharacterNames(criteria, setCharacterNameError);
+  }, 500);
 
-    const submitForm = handleSubmit(async (data) => {
-        navigate({ to: "/profile", replace: true });
-        console.log(data);
-    });
+  const submitForm = handleSubmit(async (data) => {
+    navigate({ to: "/profile", replace: true });
+    console.log(data);
+  });
 
-    return (
-        <div className="main__container">
-            <div>
-                <div className="title">Select Maplestory Job Type</div>
-                <div className="title">Fill in the mandatory fields below</div>
+  return (
+    <div className="main__container">
+      <div>
+        <div className="title">Select Maplestory Job Type</div>
+        <div className="title">Fill in the mandatory fields below</div>
 
-                <Form<FormSchemaType>
-                    methods={methods}
-                    useLocalStorage={true}
-                    preventLeaving={true}
-                    formSchema={formSchema}
-                >
-                    <Form.ChipSelection<FormSchemaType["job"]>
-                        selections={["HERO", "ADVENTURER", "RESISTANCE"]}
-                        name={"job"}
-                        control={control}
-                        disabled={isSubmitting}
-                    />
-                    <Form.TextInput
-                        disabled={isSubmitting}
-                        label={"Character Name"}
-                        control={control}
-                        name={"characterName"}
-                        customOnChange={debouncedSearch}
-                        className={"input"}
-                        error={
-                            characterNameError && (
-                                <a href={"/profile"}>
-                                    This name exists, did you mean this guy?
-                                </a>
-                            )
-                        }
-                    />
-                    <Form.TextArea
-                        disabled={isSubmitting}
-                        name={"bio"}
-                        label={"Bio for Character"}
-                        control={control}
-                        className={"input"}
-                    />
-                    <Form.TextInput
-                        disabled={isSubmitting}
-                        label={"Password"}
-                        control={control}
-                        name={"password"}
-                        className={"input"}
-                        type={"password"}
-                    />
-                    <Form.TextInput
-                        disabled={isSubmitting}
-                        label={"Confirm Password"}
-                        control={control}
-                        name={"confirmPassword"}
-                        className={"input"}
-                        type={"password"}
-                    />
-                    <Button
-                        disabled={!isValid}
-                        onClick={submitForm}
-                        className="create__btn"
-                    >
-                        Create Character
-                    </Button>
-                </Form>
-            </div>
-        </div>
-    );
+        <Form methods={methods} useLocalStorage={true} preventLeaving={true}>
+          <Form.ChipSelection<FormSchemaType["job"]>
+            selections={["HERO", "ADVENTURER", "RESISTANCE"]}
+            name={"job"}
+            control={control}
+            disabled={isSubmitting}
+          />
+          <Form.TextInput
+            disabled={isSubmitting}
+            label={"Character Name"}
+            control={control}
+            name={"characterName"}
+            customOnChange={debouncedSearch}
+            className={"input"}
+            error={
+              characterNameError && (
+                <a href={"/profile"}>
+                  This name exists, did you mean this guy?
+                </a>
+              )
+            }
+          />
+          <Form.TextArea
+            disabled={isSubmitting}
+            name={"bio"}
+            label={"Bio for Character"}
+            control={control}
+            className={"input"}
+          />
+          <Form.TextInput
+            disabled={isSubmitting}
+            label={"Password"}
+            control={control}
+            name={"password"}
+            className={"input"}
+            type={"password"}
+          />
+          <Form.TextInput
+            disabled={isSubmitting}
+            label={"Confirm Password"}
+            control={control}
+            name={"confirmPassword"}
+            className={"input"}
+            type={"password"}
+          />
+          <Button
+            disabled={!isValid}
+            onClick={submitForm}
+            className="create__btn"
+          >
+            Create Character
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default CreateProfile;
