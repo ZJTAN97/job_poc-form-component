98f7cb5cf2b6be5b67e32432272cfcfeda8bac9c
ZJ <zhijietan97@gmail.com>
2022-10-28 15:03:16 +0800

    MAJOR REFACTOR: removed CRA dependencies, used personal webpack config

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.tsx"),
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    modules: ["src", "node_modules"],
+    fallback: {
+      net: false,
+    },
+  },
+  mode: "development",
+  devtool: "cheap-module-source-map",
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "..", "./build"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./public/index.html"),
+    }),
+  ],
+};
