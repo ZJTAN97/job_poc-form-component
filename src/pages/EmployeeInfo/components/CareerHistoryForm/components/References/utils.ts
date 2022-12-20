738a6b26d6570bd396902d76335d9106bb6aa345
ZJ <zhijietan97@gmail.com>
2022-12-20 16:18:57 +0800

    add: crud hooks for sources and references

--- src/pages/EmployeeInfo/components/CareerHistoryForm/components/References/utils.ts
+++ src/pages/EmployeeInfo/components/CareerHistoryForm/components/References/utils.ts
@@ -1,13 +1,22 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
 import {
   FieldValues,
   Path,
+  useFieldArray,
   UseFieldArrayReturn,
+  useForm,
+  useFormContext,
   UseFormReturn,
 } from "react-hook-form";
 import { AppointmentType } from "../../../../../../model/career/Appointment";
 import { CareerType } from "../../../../../../model/career/Career";
 import { CertificationType } from "../../../../../../model/career/Certification";
-import { ReferenceType } from "../../../../../../model/common/Reference";
+import {
+  Reference,
+  ReferenceType,
+} from "../../../../../../model/common/Reference";
+import { Source, SourceType } from "../../../../../../model/common/Source";
 
 export const useExistingReference = <T extends FieldValues>({
   references,
@@ -179,32 +188,172 @@ export const setReferences = ({
 
 export const useSetSources = ({
   fieldName,
-  formContext,
-  arrayMethod,
   existingReference,
-  referenceForm,
   currentArrayId,
-  sourceId,
 }: {
   fieldName: Path<CareerType> | Path<AppointmentType> | Path<CertificationType>;
-  formContext: UseFormReturn<CareerType>;
-  arrayMethod: UseFieldArrayReturn<CareerType>;
   existingReference?: ReferenceType;
-  referenceForm: UseFormReturn<ReferenceType>;
   currentArrayId?: number;
-  sourceId?: number;
 }) => {
-  const saveOrUpdateSource = () => {
-    console.info(
-      `[INFO] Creating/Updating ${fieldName} and array Id ${currentArrayId}`,
-    );
+  const formContext = useFormContext<CareerType>();
+
+  const [sourceId, setSourceId] = React.useState<number>();
+
+  const referenceFormMethod = useForm<ReferenceType>({
+    resolver: zodResolver(Reference),
+    mode: "onChange",
+    defaultValues: {
+      field: fieldName,
+      content: "",
+      sources: [],
+    },
+  });
+
+  const sourceFormMethod = useForm<SourceType>({
+    resolver: zodResolver(Source),
+    mode: "onChange",
+    defaultValues: {
+      comment: "",
+      dateObtained: "2022-11-11T12:19:54.52",
+      referenceType: undefined,
+    },
+  });
+
+  const referenceArrayMethods = useFieldArray<CareerType>({
+    control: formContext.control,
+    name: useCurrentReference(fieldName),
+  });
+
+  const sourceArrayMethods = useFieldArray<ReferenceType>({
+    control: referenceFormMethod.control,
+    name: "sources",
+  });
+
+  const updateReferences = () => {
+    const isAppointmentReference =
+      fieldName === "rank" || fieldName === "position";
+    const isCertReference = fieldName === "issuedBy" || fieldName === "name";
+    const isSkillReference = fieldName === "skills" && existingReference;
+
+    if (isAppointmentReference) {
+      if (existingReference) {
+        const existingReferenceId = formContext
+          .getValues()
+          .appointment.references.indexOf(existingReference);
+        referenceArrayMethods.update(
+          existingReferenceId,
+          referenceFormMethod.getValues(),
+        );
+      } else {
+        referenceArrayMethods.append(referenceFormMethod.getValues());
+      }
+    } else if (isCertReference) {
+      const selectedCert =
+        formContext.getValues().certsToField[currentArrayId!];
+      if (existingReference) {
+        const selectedCertReferenceId =
+          selectedCert.references.indexOf(existingReference);
+        let currentReferences = selectedCert.references;
+        currentReferences[selectedCertReferenceId] =
+          referenceFormMethod.getValues();
+        referenceArrayMethods.update(currentArrayId!, {
+          ...selectedCert,
+          references: currentReferences,
+        });
+      } else {
+        referenceArrayMethods.update(currentArrayId!, {
+          ...selectedCert,
+          references: [
+            ...selectedCert.references,
+            referenceFormMethod.getValues(),
+          ],
+        });
+      }
+    } else if (isSkillReference) {
+      const existingReferenceId = formContext
+        .getValues()
+        .references.indexOf(existingReference);
+      referenceArrayMethods.update(existingReferenceId, {
+        ...existingReference,
+        sources: referenceFormMethod.getValues().sources,
+      });
+    } else {
+      if (existingReference) {
+        const existingReferenceId = formContext
+          .getValues()
+          .references.indexOf(existingReference);
+        referenceArrayMethods.update(
+          existingReferenceId,
+          referenceFormMethod.getValues(),
+        );
+      } else {
+        referenceArrayMethods.append(referenceFormMethod.getValues());
+      }
+    }
   };
+
+  const applySourcesToReferences = () => {
+    if (sourceId !== undefined) {
+      sourceArrayMethods.update(sourceId, sourceFormMethod.getValues());
+      setSourceId(undefined);
+    } else {
+      sourceArrayMethods.append(sourceFormMethod.getValues());
+    }
+    updateReferences();
+    sourceFormMethod.reset();
+  };
+
+  const editSource = () => {
+    if (sourceId) {
+      sourceFormMethod.setValue(
+        "referenceType",
+        referenceFormMethod.getValues().sources[sourceId].referenceType,
+      );
+      sourceFormMethod.setValue(
+        "dateObtained",
+        referenceFormMethod.getValues().sources[sourceId].dateObtained,
+      );
+      sourceFormMethod.setValue(
+        "comment",
+        referenceFormMethod.getValues().sources[sourceId].comment,
+      );
+    }
+  };
+
   const deleteSource = () => {
-    console.info(`[INFO] Deleting ${fieldName} and array Id ${currentArrayId}`);
+    if (existingReference) {
+      sourceArrayMethods.remove(sourceId);
+      const referenceId = formContext
+        .getValues()
+        .references.indexOf(existingReference);
+      referenceArrayMethods.update(
+        referenceId,
+        referenceFormMethod.getValues(),
+      );
+
+      if (referenceFormMethod.getValues().sources.length === 0) {
+        if (
+          (fieldName === "name" || fieldName === "issuedBy") &&
+          currentArrayId
+        ) {
+          const selectedCert =
+            formContext.getValues().certsToField[currentArrayId];
+          referenceArrayMethods.update(currentArrayId, {
+            ...selectedCert,
+            references: [],
+          });
+        } else {
+          referenceArrayMethods.remove(referenceId);
+        }
+      }
+    }
   };
 
   return {
-    saveOrUpdateSource,
+    setSourceId,
+    sourceFormMethod,
+    applySourcesToReferences,
+    editSource,
     deleteSource,
   };
 };
